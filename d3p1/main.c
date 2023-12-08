a7116154d77870763751e96a756c5607e3a22dff
tnebes <tomislav.nebes@gmail.com>
2023-12-08 14:25:10 +0100

    Added parsing of lines

--- d3p1/main.c
+++ d3p1/main.c
@@ -2,19 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINES 1024
+
+enum CharacterType
+{
+   EMPTY_SPACE,
+   DIGIT,
+   SYMBOL
+};
+
+typedef struct {
+   int lineLength;
+   int *numbers;
+   int *symbols;
+} Line;
+
 int isDigit(const char character)
 {
    return (character >= '0' && character <= '9');
 }
 
-int isJunk(const char character)
+int isSymbol(const char character)
 {
-   return (character == ' ' || character == ',');
+   return (character != '.' && !isDigit(character));
 }
 
-int isEnd(const char character)
+int isEmptySpace(const char character)
 {
-   return (character == ';' || character == '\0');
+   return character == '.';
+}
+
+int classifyCharacter(const char character)
+{
+   if (isEmptySpace(character))
+   {
+      return EMPTY_SPACE;
+   }
+   if (isDigit(character))
+   {
+      return DIGIT;
+   }
+   if (isSymbol(character))
+   {
+      return SYMBOL;
+   }
+   printf("Character %c cannot be identified.\n", character);
+   exit(1);
 }
 
 int getFirstIntFromInput(const char input[], const int start, const int inputLength)
@@ -61,6 +94,97 @@ int getPositionOfCharacter(const char input[], const int start, const int end, c
    return -1;
 }
 
+Line *initialiseLine(const int lineLength)
+{
+   Line *line = malloc(sizeof(Line));
+
+   if (line == NULL)
+   {
+      printf("Failed to allocate memory for Line\n");
+      exit(1);
+   }
+
+   line->lineLength = lineLength;
+   line->numbers = malloc(sizeof(int) * lineLength);
+   line->symbols = malloc(sizeof(int) * lineLength);
+   
+   for (int i = 0; i < lineLength; i++)
+   {
+      line->numbers[i] = 0;
+      line->symbols[i] = 0;
+   }
+
+   return line;
+}
+
+void freeLine(Line *line)
+{
+   free(line->numbers);
+   free(line->symbols);
+   free(line);
+}
+
+void freeLines(Line* lines[], const int numberOfLines)
+{
+   for (int i = 0; i < numberOfLines; i++)
+   {
+      freeLine(lines[i]);
+   }
+}
+
+int getLengthOfNumber(int number)
+{
+   int counter = 1;
+   while (number >= 10)
+   {
+      number /= 10;
+      counter++;
+   }
+   return counter;
+}
+
+void populateLine(Line *line, const char* input, const int inputLength)
+{
+   int currentCharacterPosition = 0;
+   char currentCharacter = input[currentCharacterPosition];
+   while (!(currentCharacter == '\0' || currentCharacter == '\n'))
+   {
+      switch(classifyCharacter(currentCharacter))
+      {
+         int number;
+         int numberLength;
+         int targetPosition;
+         case EMPTY_SPACE:
+            currentCharacter = input[++currentCharacterPosition];
+            break;
+         case DIGIT:
+            number = getFirstIntFromInput(input, currentCharacterPosition, inputLength);
+            numberLength = getLengthOfNumber(number);
+            targetPosition = currentCharacterPosition + numberLength;
+            while (currentCharacterPosition < targetPosition)
+            {
+               line->numbers[currentCharacterPosition++] = number;
+            }
+            currentCharacter = input[++currentCharacterPosition];
+            break;
+         case SYMBOL:
+            line->symbols[currentCharacterPosition] = 1;
+            currentCharacter = input[++currentCharacterPosition];
+            break;
+         default:
+            printf("Could not classify character %c\n", currentCharacter);
+            exit(1);
+      }
+   }
+}
+
+Line *parseLine(const char* input, const int inputLength)
+{
+   Line *line = initialiseLine(inputLength);
+   populateLine(line, input, inputLength);
+   return line;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -83,12 +207,16 @@ int main(int argc, char *argv[])
 
    int sum = 0;
    char line[1024];
+   Line *lines[MAX_LINES];
+   int counter = 0;
 
    while (fgets(line, sizeof(line), file))
    {
       const int lineLength = strlen(line);
+      lines[counter++] = parseLine(line, lineLength);
    }
 
    printf("Sum is: %d", sum);
+   freeLines(lines, counter);
    return 0;
 }
